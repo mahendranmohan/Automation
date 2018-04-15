14776dacee28435d5679633e54a9cf0c490768c2
User <User@DESKTOP-DTG1AD5>
2018-04-16 02:34:53 +0530

    first try

---
+++ src/TestOpenBrowser/FirstFirefoxOpenSelenium.java
@@ -0,0 +1,30 @@
+package TestOpenBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstFirefoxOpenSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String UserDir = System.getProperty("user.dir");
+		//E:\softwares\eclipse-jee\eclipse\javaprojects\SeleniumAutomation
+		System.out.println(UserDir);
+		System.setProperty("webdriver.gecko.driver",UserDir+"\\lib\\geckodriver\\geckodriver.exe");
+		System.setProperty("webdriver.firefox.bin","C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+	
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://rediff.com/");
+		//driver.get("http://seleniumhq.org/");
+		System.out.println(driver.getTitle());
+	 //   driver.close();
+		driver.quit();
+			
+		
+		
+		
+		
+
+	}
+
+}
